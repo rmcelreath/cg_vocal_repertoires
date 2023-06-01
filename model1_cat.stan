6265982e46c154bed07a66ee9c1d7871fd37bc90
Richard McElreath <richard_mcelreath@eva.mpg.de>
2023-06-01 20:33:27 +0200

    work work

---
+++ model1_cat.stan
@@ -0,0 +1,74 @@
+// model with age covariate as categories
+data{
+    int N; // individuals
+    int M; // tokens
+    int J; // number of recordings
+    vector[J] d; // durations of each recording
+    array[J] int id; // individual
+    array[J,M] int Y; // observed token counts
+    array[N] int a; // age of each individual
+    int Na; // number of age categories
+}
+parameters {
+    vector<lower=0>[M] L; // rates of each token, conditional on individual possessing it
+    array[M,Na] real<lower=0,upper=1> p; // prob of each vocalization
+}
+model{
+    L ~ exponential(1);
+    for ( i in 1:Na ) p[1:M,i] ~ beta(2,2);
+    // loop over recordings
+    for ( j in 1:J ) {
+        for ( m in 1:M ) {
+            // compute vocalization probs for individual with this a[id[j]]
+            real log_pa = log(p[m,a[id[j]]]);
+            if ( Y[j,m] >0 ) {
+                // observed non-zero count, estimate rate
+                Y[j,m] ~ poisson( L[m] * d[j] );
+                target += log_pa;
+            } else {
+                // observed zero so account for mixture
+                target += log_sum_exp( 
+                    log_pa + poisson_lpmf(0|L[m]*d[j]) , // has it, but produced zero
+                    log1m_exp(log_pa) // doesn't have it
+                );
+            }
+        }//m
+    }//j
+}
+generated quantities {
+    // calculate posterior prob of each token for each individual in sample
+    // this allows estimation of undercounting due to sampling and rate variation
+    matrix[N,M] phi;
+    vector[N] R; // repertiore size
+    for ( i in 1:N ) {
+        vector[2] pp;
+        real Z;
+        for ( m in 1:M ) {
+            // Pr(p[m]==1|Y) = Pr(Y|p[m]==1) p[m] / Z
+            // Z = Pr(Y|p[m]==1) p[m] + Pr(Y|p[m]==0) (1-p[m])
+            // need to sum Y across all recordings for individual i
+            // need sum duration of these recordings as well
+            real dd = 0;
+            real yy = 0;
+            for ( j in 1:J ) {
+                if ( id[j]==i ) {
+                    dd += d[j];
+                    yy += Y[j,m];
+                }
+            }//j
+
+            if ( yy>0 )
+                // individual has it for sure, so no calculation needed
+                phi[i,m] = 1;
+            else {
+                // bayes it up
+                real log_pa = log(p[m,a[i]]);
+                pp[1] = log_pa + poisson_lpmf(0|L[m]*dd);
+                pp[2] = log1m_exp(log_pa);
+                Z = log_sum_exp( pp[1] , pp[2] );
+                phi[i,m] = exp( pp[1] - Z );
+            }
+        }//m
+        R[i] = sum( bernoulli_rng( phi[i,] ) );
+    }//i
+}
--- simulation.r
+++ simulation.r
@@ -4,70 +4,103 @@ library(rethinking)
 
 # synthetic data
 
-N <- 50 # number of individuals
-M <- 50 # number of unique vocalizations
-p <- sort(runif(M)) # prob of each vocalization, ordered rare to common, among individuals
-L <- rlnorm(M,0.25,0.5)/10 # rate of each vocalization, when individual uses it
+sim_repertoire <- function(
+    N = 50, # number of individuals
+    M = 50, # number of unique vocalizations
+    NR = 20, # average recordings per individual
+    age_model = "NULL",
+    beta = 2,
+    age
+) {
+        
+    p <- sort(rbeta(M,5,2)) # prob of each vocalization, ordered rare to common, among individuals
+    L <- rlnorm(M,0.25,0.5)/10 # rate of each vocalization, when individual uses it
 
-# age as individual covariate
-a <- runif(N)
-beta <- 2 # rate of increase with age
-alpha <- p # asymptotic probabilities of each 
-# curve( 0.6*(1-exp(-beta*x)) , from=0 , to=1 , ylim=c(0,1) )
+    # simulate individual repertoires
+    # individuals in rows, tokens in columns
+    x <- t( replicate( N , rbern(M,prob=p) ) )
 
-# simulate individual repertoires
-# individuals in rows, tokens in columns
-x <- t( replicate( N , rbern(M,prob=p) ) )
-for ( i in 1:N ) {
-    x[i,] <- rbern( M , prob=alpha*(1-exp(-beta*a[i])) )
-}#i
+    # age as individual covariate
+    if ( age_model=="monotonic" ) {
+        # continuous age
+        if ( missing(age) )
+            a <- runif(N)
+        else
+            a <- age
+        alpha <- p # asymptotic probabilities of each 
+        # curve( 0.6*(1-exp(-beta*x)) , from=0 , to=1 , ylim=c(0,1) )
+        for ( i in 1:N ) {
+            x[i,] <- rbern( M , prob=alpha*(1-exp(-beta*a[i])) )
+        }#i
+    }
 
-# simulate vocal samples with varying sample sizes for each individual
-max_tokens <- 20 # max number of tokens per recording
-n_recordings <- rpois(N,20) + 1
-id <- rep(1:N,times=n_recordings) # individual making each recording
-y <- matrix(NA,sum(n_recordings),max_tokens)
-d <- 0.2 * rpois(sum(n_recordings),5) + 1 # durations
+    if ( age_model=="cat" ) {
+        # categorical age
+        if ( missing(age) )
+            a <- runif(N)
+        else
+            a <- age
+        a_cat <- ifelse( a < 0.25 , 1 , a )
+        a_cat <- ifelse( a < 0.5 & a > 0.25 , 2 , a_cat )
+        a_cat <- ifelse( a > 0.5 , 3 , a_cat )
+        a <- a_cat
+    }
 
-if ( FALSE ) {
-# use Gillespie algorithm to simulate vocalizations
-# this gives intervals between tokens, but we don't need that right now
-for ( i in 1:sum(n_recordings) ) {    
-    t0 <- 0 # time in recording
-    j <- 1
-    while( (t0 < d[i]) & (j <= max_tokens) ) {
-        r <- runif(2)
-        R <- sum(L*x[id[i],]) # total rate of vocalizations
-        t1 <- (1/R)*log(1/r[1]) # time to next vocalization
-        # which vocalization?
-        k <- L*x[id[i],] # rates for this individual
-        y[i,j] <- sample(1:M,size=1,prob=k) # draw 1 proportional to relative rates
-        # update
-        t0 <- t1
-        j <- j + 1
-        # check for max_tokens breech and truncate recording at this point
-        if ( j > max_tokens ) d[i] <- t1
-    }#t0 < d
-}#i
+    # simulate vocal samples with varying sample sizes for each individual
+    max_tokens <- 20 # max number of tokens per recording
+    n_recordings <- rpois(N,NR) + 1
+    id <- rep(1:N,times=n_recordings) # individual making each recording
+    y <- matrix(NA,sum(n_recordings),max_tokens)
+    d <- 0.2 * rpois(sum(n_recordings),5) + 1 # durations
+
+    if ( FALSE ) {
+    # use Gillespie algorithm to simulate vocalizations
+    # this gives intervals between tokens, but we don't need that right now
+    for ( i in 1:sum(n_recordings) ) {    
+        t0 <- 0 # time in recording
+        j <- 1
+        while( (t0 < d[i]) & (j <= max_tokens) ) {
+            r <- runif(2)
+            R <- sum(L*x[id[i],]) # total rate of vocalizations
+            t1 <- (1/R)*log(1/r[1]) # time to next vocalization
+            # which vocalization?
+            k <- L*x[id[i],] # rates for this individual
+            y[i,j] <- sample(1:M,size=1,prob=k) # draw 1 proportional to relative rates
+            # update
+            t0 <- t1
+            j <- j + 1
+            # check for max_tokens breech and truncate recording at this point
+            if ( j > max_tokens ) d[i] <- t1
+        }#t0 < d
+    }#i
+    }
+
+    # count number of each token in each recording
+    XX <- matrix(NA,sum(n_recordings),M)
+    for ( i in 1:sum(n_recordings) ) {
+        for ( j in 1:M )
+            XX[i,] <- rpois( M , L*x[id[i],]*d[i] )
+    }#i
+
+    dat <- list(
+        N=N, # number of individuals
+        M=M, # number of unique vocalizations
+        J=sum(n_recordings), # number of recordings (all individuals)
+        d=d, # durations of each recording
+        id=id, # individual IDs, length J
+        Y=XX, # matrix with recordings on rows and counts of vocalizations in columns
+        a=a, # age covariate
+        L=L, # rates
+        p=p, # base rates
+        Rtrue=x ) # true repertoires 
+
+    return(dat)
 }
-
-# count number of each token in each recording
-XX <- matrix(NA,sum(n_recordings),M)
-for ( i in 1:sum(n_recordings) ) {
-    for ( j in 1:M )
-        XX[i,] <- rpois( M , L*x[id[i],]*d[i] )
-}#i
 
 ################################################################################
 # estimate - basic no covariate model
 
-dat <- list(
-    N=N, # number of individuals
-    M=M, # number of unique vocalizations
-    J=sum(n_recordings), # number of recordings (all individuals)
-    d=d, # durations of each recording
-    id=id, # individual IDs, length J
-    Y=XX ) # matrix with recordings on rows and counts of vocalizations in columns
+dat <- sim_repertoire(N=100,M=100,NR=10)
 
 m0 <- cstan(file="model0.stan",data=dat,chains=1)
 
@@ -77,10 +110,10 @@ post <- extract.samples(m0)
 # blank2(w=2)
 par(mfrow=c(1,2))
 
-plot( L , apply(post$L,2,mean) , xlab="true rate" , ylab="posterior mean rate" , col=2 , lwd=2 )
+plot( dat$L , apply(post$L,2,mean) , xlab="true rate" , ylab="posterior mean rate" , col=2 , lwd=2 )
 abline(a=0,b=1)
 
-plot( p , apply(post$p,2,mean) , xlab="true prob" , ylab="posterior mean prob" , col=2 , lwd=2 )
+plot( dat$p , apply(post$p,2,mean) , xlab="true prob" , ylab="posterior mean prob" , col=2 , lwd=2 )
 abline(a=0,b=1)
 
 # individual repertoires
@@ -89,20 +122,20 @@ phi <- apply(post$phi,2:3,mean)
 # round(phi,2)
 
 par(mfrow=c(1,2))
-image(x)
+image(dat$Rtrue)
 image(phi)
 
 # compare estimated repertoire size to observed
-Robs = rep(0,N)
-Rtrue = rep(0,N)
-for ( i in 1:N ) {
-    for ( m in 1:M ) {
+Robs = rep(0,dat$N)
+Rtrue = rep(0,dat$N)
+for ( i in 1:dat$N ) {
+    for ( m in 1:dat$M ) {
         # does individual i ever do m?
-        ii <- which(id==i)
-        if ( any( XX[ii,m] > 0 ) )
+        ii <- which(dat$id==i)
+        if ( any( dat$Y[ii,m] > 0 ) )
             Robs[i] = Robs[i] + 1
     }#m
-    Rtrue[i] = sum(x[i,])
+    Rtrue[i] = sum(dat$Rtrue[i,])
 }#i
 
 par(mfrow=c(1,2))
@@ -113,16 +146,27 @@ plot(Rtrue,apply(post$R,2,mean),xlab="true repertoire size",ylab="posterior mean
 abline(a=0,b=1)
 
 ######################################################################################
-# covariate model
+# categorical covariate model
 
 dat <- list(
-    N=N,
-    M=M,
-    J=sum(n_recordings),
-    d=d,
-    id=id,
-    Y=XX,
-    a=a )
+    N=N, # number of individuals
+    M=M, # number of unique vocalizations
+    J=sum(n_recordings), # number of recordings (all individuals)
+    d=d, # durations of each recording
+    id=id, # individual IDs, length J
+    Y=XX, # matrix with recordings on rows and counts of vocalizations in columns
+    a=a_cat,
+    Na=3 ) 
+
+m0 <- cstan(file="model1_cat.stan",data=dat,chains=1)
+
+precis(m0,2)
+post <- extract.samples(m0)
+
+######################################################################################
+# covariate model
+
+dat <- sim_repertoire(N=50,M=50,NR=10,age_model="monotonic",beta=2)
 
 m0 <- cstan(file="model1.stan",data=dat,chains=1)
 
@@ -132,10 +176,10 @@ post <- extract.samples(m0)
 # blank2(w=2)
 par(mfrow=c(1,3))
 
-plot( L , apply(post$L,2,mean) , xlab="true rate" , ylab="posterior mean rate" , col=2 , lwd=2 )
+plot( dat$L , apply(post$L,2,mean) , xlab="true rate" , ylab="posterior mean rate" , col=2 , lwd=2 )
 abline(a=0,b=1)
 
-plot( alpha , apply(post$alpha,2,mean) , xlab="true prob" , ylab="posterior mean prob" , col=2 , lwd=2 )
+plot( dat$p , apply(post$alpha,2,mean) , xlab="true prob" , ylab="posterior mean prob" , col=2 , lwd=2 )
 abline(a=0,b=1)
 
 dens(post$beta)
@@ -146,20 +190,20 @@ phi <- apply(post$phi,2:3,mean)
 # round(phi,2)
 
 par(mfrow=c(1,2))
-image(x)
+image(dat$Rtrue)
 image(phi)
 
 # compare estimated repertoire size to observed
-Robs = rep(0,N)
-Rtrue = rep(0,N)
-for ( i in 1:N ) {
-    for ( m in 1:M ) {
+Robs = rep(0,dat$N)
+Rtrue = rep(0,dat$N)
+for ( i in 1:dat$N ) {
+    for ( m in 1:dat$M ) {
         # does individual i ever do m?
-        ii <- which(id==i)
-        if ( any( XX[ii,m] > 0 ) )
+        ii <- which(dat$id==i)
+        if ( any( dat$Y[ii,m] > 0 ) )
             Robs[i] = Robs[i] + 1
     }#m
-    Rtrue[i] = sum(x[i,])
+    Rtrue[i] = sum(dat$Rtrue[i,])
 }#i
 Rest <- apply(post$R,2,mean)
 
@@ -171,19 +215,24 @@ plot(Rtrue,Rest,xlab="true repertoire size",ylab="posterior mean size",col=2,lwd
 abline(a=0,b=1)
 
 # age against repertoire
-plot( a , Robs - Rtrue , xlim=c(0,1) , xlab="age" , ylab="repertoire size" , lwd=2 , col=2 , ylim=c(-5,5) )
-points( a , Rest - Rtrue , lwd=2 , col=1 )
+yr <- max(abs(Robs - Rtrue))
+plot( dat$a , Robs - Rtrue , xlim=c(0,1) , xlab="age" , ylab="repertoire size" , lwd=2 , col=2 , ylim=c(-yr,yr) )
+points( dat$a , Rest - Rtrue , lwd=2 , col=1 )
 pi <- apply(post$R,2,PI,0.89)
-for ( i in 1:N ) lines( rep(a[i],2) , pi[,i] - Rtrue[i] , col=1 )
+for ( i in 1:N ) lines( rep(dat$a[i],2) , pi[,i] - Rtrue[i] , col=1 )
 abline(h=0,lty=2)
 
 # functions
-plot(NULL,xlim=c(0,1),ylim=c(-0.5,0.5),xlab="age",ylab="p")
+plot(NULL,xlim=c(0,1),ylim=c(0,1),xlab="age",ylab="p")
 xseq <- seq(from=0,to=1,len=30)
-for ( m in 1:M ) {
-    pm <- sapply( xseq , function(a) mean( post$alpha[,m] * ( 1 - exp( -post$beta*a ) ) ) )
-    pm_true <- sapply( xseq , function(a) mean( alpha[m] * ( 1 - exp( -beta*a ) ) ) )
-    lines( xseq , pm - pm_true , lwd=2 )
+for ( m in M:M ) {
+    for ( s in 1:100 ) {
+        pm <- sapply( xseq , function(a) mean( post$alpha[s,m] * ( 1 - exp( -post$beta[s]*a ) ) ) )
+        lines( xseq , pm , lwd=1 )
+    }
+    pm_true <- sapply( xseq , function(a) ( alpha[m] * ( 1 - exp( -beta*a ) ) ) )
+    lines( xseq , pm_true , lwd=6 , col="white" )
+    lines( xseq , pm_true , lwd=2 , col=2 )
 }
 
 ##########
