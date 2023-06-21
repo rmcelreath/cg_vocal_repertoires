7819da3b329e76f619a40065752a2b95024d17bf
Richard McElreath <richard_mcelreath@eva.mpg.de>
2023-06-21 11:37:34 +0200

    ulam model with covariates added

---
+++ model_ulam_covariates.r
@@ -0,0 +1,123 @@
+# general covariate model - sim example
+
+# function to compute repertoires post-sampling
+Rcalc <- function(fit,S=500) {
+    N <- max(fit@data$ID)
+    M <- fit@data$M
+    post <- extract.samples(fit)
+    phi <- array(NA,c(S,N,M))
+    R <- array(NA,c(S,N))
+    for ( n in 1:N ) {
+        for ( m in 1:M ) {
+            # sum all m observations for individual n
+            dm <- 0 # sum duration
+            ym <- 0 # sum count
+            for ( i in 1:length(fit@data$Y) ) {
+                if ( fit@data$ID[i]==n & fit@data$BID[i]==m ) {
+                    dm <- dm + fit@data$D[ fit@data$J[i] ]
+                    ym <- ym + fit@data$Y[i]
+                }
+            }#i
+            # now invert probability
+            if ( ym > 0 ) {
+                # observed
+                phi[,n,m] <- rep(1,S)
+            } else {
+                # bayes
+                # find a row i for this individual and behavior, so we fetch right p for its covariates
+                idx <- which(fit@data$ID==n & fit@data$BID==m)[1]
+                # compute
+                pp1 <- log(post$p[,idx]) + dpois(0,post$L[,m]*dm,log=TRUE)
+                pp2 <- log1p(-post$p[,idx])
+                Z <- rep(NA,S)
+                for ( s in 1:S )
+                    Z[s] <- log_sum_exp( c( pp1[s] , pp2[s] ) )
+                phi[,n,m] <- exp( pp1 - Z )
+            }
+        }#m
+        for ( s in 1:S ) R[s,n] <- sum( rbern(M,prob=phi[s,n,]) )
+    }#n
+    return(list(phi=phi,R=R))
+}
+
+# covariates for each individual
+N <- 50
+M <- 50
+age <- sample(1:3,size=N,replace=TRUE)
+sex <- sample(1:2,size=N,replace=TRUE)
+p_base <- rbeta(M,2,2)
+p_sim <- matrix(NA,nrow=N,ncol=M)
+for ( n in 1:N ) {
+    p_sim[n,] <- inv_logit(
+        logit(p_base) + 0.5*(age[n]-1) + 0.5*(sex[n]-1)
+    )
+}
+
+dat <- sim_repertoire(N=N,M=M,NR=10,age_model="X",p=p_sim)
+
+# convert Y matrix to long form with m column
+YY <- rep(NA,length(dat$Y))
+MM <- YY
+ID <- YY
+JJ <- YY
+k <- 1
+for ( j in 1:nrow(dat$Y) ) for ( m in 1:ncol(dat$Y) ) {
+    YY[k] <- dat$Y[j,m]
+    MM[k] <- m
+    ID[k] <- dat$id[j]
+    JJ[k] <- j
+    k <- k + 1
+}
+
+dat_list <- list(
+    Y = YY,    # behavior counts
+    ID = ID,   # individual ID
+    BID = MM,  # behavior ID
+    J = JJ,    # recording ID
+    D = dat$d, # duration of recording,
+    M = max(MM),
+    age=age,
+    sex=sex
+)
+
+m0u <- ulam(
+    alist(
+        # Y > 0
+        Y|Y>0 ~ custom( log(p) + poisson_log_lpmf(Y|log_lambda1) ),
+        # Y = 0
+        Y|Y==0 ~ custom( log_sum_exp( 
+            log(p) + poisson_log_lpmf(0|log_lambda1) , 
+            log1m(p)
+         ) ),
+        # models for rate of behavior and prob possess behavior
+        log_lambda1 <- log(L[BID]) + log(D[J]),
+        save> logit(p) <- a[BID] + B[age[ID],sex[ID]],
+        # priors
+        vector[M]:L ~ exponential(1),
+        vector[M]:a ~ normal(0,1),
+        matrix[3,2]:B ~ normal(0,0.5)
+    ), data=dat_list , chains=4 , cores=4 , sample=TRUE , iter=500 )
+
+precis(m0u,3,pars=c("B"))
+
+r <- Rcalc(m0u,S=1000)
+
+# compare estimated repertoire size to observed
+Robs = rep(0,dat$N)
+Rtrue = rep(0,dat$N)
+for ( i in 1:dat$N ) {
+    for ( m in 1:dat$M ) {
+        # does individual i ever do m?
+        ii <- which(dat$id==i)
+        if ( any( dat$Y[ii,m] > 0 ) )
+            Robs[i] = Robs[i] + 1
+    }#m
+    Rtrue[i] = sum(dat$Rtrue[i,])
+}#i
+
+par(mfrow=c(1,2))
+plot(Rtrue,Robs,xlab="true repertoire size",ylab="observed size",col=2,lwd=2)
+abline(a=0,b=1)
+
+plot(Rtrue,apply(r$R,2,mean),xlab="true repertoire size",ylab="posterior mean size",col=2,lwd=2)
+abline(a=0,b=1)
--- simulation.r
+++ simulation.r
@@ -10,15 +10,23 @@ sim_repertoire <- function(
     NR = 20, # average recordings per individual
     age_model = "NULL",
     beta = 2,
-    age
+    age,
+    p,L,
+    X
 ) {
-        
-    p <- sort(rbeta(M,5,2)) # prob of each vocalization, ordered rare to common, among individuals
-    L <- rlnorm(M,0.25,0.5)/10 # rate of each vocalization, when individual uses it
+
+    a <- NA
+    
+    if ( missing(p) )
+        p <- sort(rbeta(M,5,2)) # prob of each vocalization, ordered rare to common, among individuals
+    if ( missing(L) )
+        L <- rlnorm(M,0.25,0.5)/10 # rate of each vocalization, when individual uses it
 
     # simulate individual repertoires
     # individuals in rows, tokens in columns
-    x <- t( replicate( N , rbern(M,prob=p) ) )
+    x <- matrix(NA,nrow=N,ncol=M)
+    if ( age_model=="NULL" )
+        x <- t( replicate( N , rbern(M,prob=p) ) )
 
     # age as individual covariate
     if ( age_model=="monotonic" ) {
@@ -40,10 +48,20 @@ sim_repertoire <- function(
             a <- runif(N)
         else
             a <- age
+        for ( i in 1:N ) {
+            x[i,] <- rbern( M , prob=p*(1-exp(-beta*a[i])) )
+        }#i
         a_cat <- ifelse( a < 0.25 , 1 , a )
         a_cat <- ifelse( a < 0.5 & a > 0.25 , 2 , a_cat )
         a_cat <- ifelse( a > 0.5 , 3 , a_cat )
         a <- a_cat
+    }
+
+    # general categorical covariates, table X supplied for individual covariates, p matrix supplied as argument in which X values already used to generate p values for each individual (set of covariates)
+    if ( age_model=="X" ) {
+        for ( i in 1:N ) {
+            x[i,] <- rbern( M , prob=p[i,] )
+        }#i
     }
 
     # simulate vocal samples with varying sample sizes for each individual
@@ -100,7 +118,8 @@ sim_repertoire <- function(
 ################################################################################
 # estimate - basic no covariate model
 
-dat <- sim_repertoire(N=100,M=100,NR=10)
+dat <- sim_repertoire(N=50,M=50,NR=10)
+dat$a <- NULL
 
 m0 <- cstan(file="model0.stan",data=dat,chains=1)
 
@@ -145,23 +164,142 @@ abline(a=0,b=1)
 plot(Rtrue,apply(post$R,2,mean),xlab="true repertoire size",ylab="posterior mean size",col=2,lwd=2)
 abline(a=0,b=1)
 
+# ulam version with post-sampling computaiton of repertoires
+# convert Y matrix to long form with m column
+YY <- rep(NA,length(dat$Y))
+MM <- YY
+ID <- YY
+JJ <- YY
+k <- 1
+for ( j in 1:nrow(dat$Y) ) for ( m in 1:ncol(dat$Y) ) {
+    YY[k] <- dat$Y[j,m]
+    MM[k] <- m
+    ID[k] <- dat$id[j]
+    JJ[k] <- j
+    k <- k + 1
+}
+
+dat_list <- list(
+    Y = YY,    # behavior counts
+    ID = ID,   # individual ID
+    BID = MM,  # behavior ID
+    J = JJ,    # recording ID
+    D = dat$d, # duration of recording,
+    M = max(MM)
+)
+
+m0u <- ulam(
+    alist(
+        # Y > 0
+        Y|Y>0 ~ custom( log(p[BID]) + poisson_log_lpmf(Y|log_lambda1) ),
+        # Y = 0
+        Y|Y==0 ~ custom( log_sum_exp( 
+            log(p[BID]) + poisson_log_lpmf(0|log_lambda1) , 
+            log1m(p[BID])
+         ) ),
+        # log-linear model for rate of behavior
+        log_lambda1 <- log(L[BID]) + log(D[J]),
+        # priors
+        vector[M]:L ~ exponential(1),
+        vector[M]:p ~ beta(2,2)
+    ), data=dat_list , chains=1 , sample=TRUE )
+
+postu <- extract.samples(m0u)
+
+# function to compute repertoires
+Rcalc <- function(fit) {
+    N <- max(fit@data$ID)
+    M <- fit@data$M
+    post <- extract.samples(fit)
+    S <- dim(post$p)[1]
+    R <- array(NA,c(N,M,S))
+    for ( n in 1:N ) {
+        for ( m in 1:M ) {
+            # sum all m observations for individual n
+            dm <- 0 # sum duration
+            ym <- 0 # sum count
+            for ( i in 1:length(fit@data$Y) ) {
+                if ( fit@data$ID[i]==n & fit@data$BID[i]==m ) {
+                    dm <- dm + fit@data$D[ fit@data$J[i] ]
+                    ym <- ym + fit@data$Y[i]
+                }
+            }#i
+            # now invert probability
+            if ( ym > 0 ) {
+                # observed
+                R[n,m,] <- 1
+            } else {
+                # bayes
+                pp1 <- log(post$p[,m]) + dpois(0,post$L[,m]*dm,log=TRUE);
+                pp2 <- log1p(-post$p[,m]);
+                Z <- rep(NA,S)
+                for ( s in 1:S )
+                    Z[s] <- log_sum_exp( c( pp1[s] , pp2[s] ) );
+                R[n,m,] <- exp( pp1 - Z );
+            }
+        }#m
+    }#n
+    return(R)
+}
+
+phiu <- Rcalc(m0u)
+phi <- post$phi
+plot( apply(phi,2:3,mean) , apply(phiu,1:2,mean) , xlab="stan" , ylab="ulam" , col=2 , lwd=2 )
+abline(a=0,b=1)
+
 ######################################################################################
 # categorical covariate model
 
-dat <- list(
-    N=N, # number of individuals
-    M=M, # number of unique vocalizations
-    J=sum(n_recordings), # number of recordings (all individuals)
-    d=d, # durations of each recording
-    id=id, # individual IDs, length J
-    Y=XX, # matrix with recordings on rows and counts of vocalizations in columns
-    a=a_cat,
-    Na=3 ) 
+dat <- sim_repertoire(N=50,M=50,NR=10,age_model="cat",beta=2)
+dat$Na <- 3
 
 m0 <- cstan(file="model1_cat.stan",data=dat,chains=1)
 
 precis(m0,2)
 post <- extract.samples(m0)
+
+# blank2(w=2)
+
+plot( dat$L , apply(post$L,2,mean) , xlab="true rate" , ylab="posterior mean rate" , col=2 , lwd=2 )
+abline(a=0,b=1)
+
+# individual repertoires
+phi <- apply(post$phi,2:3,mean)
+# round(phi,2)
+
+par(mfrow=c(1,2))
+image(dat$Rtrue)
+image(phi)
+
+# compare estimated repertoire size to observed
+Robs = rep(0,dat$N)
+Rtrue = rep(0,dat$N)
+for ( i in 1:dat$N ) {
+    for ( m in 1:dat$M ) {
+        # does individual i ever do m?
+        ii <- which(dat$id==i)
+        if ( any( dat$Y[ii,m] > 0 ) )
+            Robs[i] = Robs[i] + 1
+    }#m
+    Rtrue[i] = sum(dat$Rtrue[i,])
+}#i
+Rest <- apply(post$R,2,mean)
+
+par(mfrow=c(1,2))
+plot(Rtrue,Robs,xlab="true repertoire size",ylab="observed size",col=2,lwd=2)
+abline(a=0,b=1)
+
+plot(Rtrue,Rest,xlab="true repertoire size",ylab="posterior mean size",col=2,lwd=2)
+abline(a=0,b=1)
+
+# age against repertoire
+yr <- max(abs(Robs - Rtrue))
+plot( dat$a , Robs - Rtrue , xlim=c(0,4) , xlab="age" , ylab="repertoire size" , lwd=2 , col=2 , ylim=c(-yr,yr) )
+points( dat$a , Rest - Rtrue , lwd=2 , col=1 )
+pi <- apply(post$R,2,PI,0.89)
+for ( i in 1:dat$N ) lines( rep(dat$a[i],2) , pi[,i] - Rtrue[i] , col=1 )
+abline(h=0,lty=2)
+
 
 ######################################################################################
 # covariate model
@@ -183,7 +321,7 @@ plot( dat$p , apply(post$alpha,2,mean) , xlab="true prob" , ylab="posterior mean
 abline(a=0,b=1)
 
 dens(post$beta)
-abline(v=beta,col=2)
+abline(v=2,col=2)
 
 # individual repertoires
 phi <- apply(post$phi,2:3,mean)
@@ -219,18 +357,18 @@ yr <- max(abs(Robs - Rtrue))
 plot( dat$a , Robs - Rtrue , xlim=c(0,1) , xlab="age" , ylab="repertoire size" , lwd=2 , col=2 , ylim=c(-yr,yr) )
 points( dat$a , Rest - Rtrue , lwd=2 , col=1 )
 pi <- apply(post$R,2,PI,0.89)
-for ( i in 1:N ) lines( rep(dat$a[i],2) , pi[,i] - Rtrue[i] , col=1 )
+for ( i in 1:dat$N ) lines( rep(dat$a[i],2) , pi[,i] - Rtrue[i] , col=1 )
 abline(h=0,lty=2)
 
 # functions
 plot(NULL,xlim=c(0,1),ylim=c(0,1),xlab="age",ylab="p")
 xseq <- seq(from=0,to=1,len=30)
-for ( m in M:M ) {
+for ( m in dat$M:dat$M ) {
     for ( s in 1:100 ) {
         pm <- sapply( xseq , function(a) mean( post$alpha[s,m] * ( 1 - exp( -post$beta[s]*a ) ) ) )
         lines( xseq , pm , lwd=1 )
     }
-    pm_true <- sapply( xseq , function(a) ( alpha[m] * ( 1 - exp( -beta*a ) ) ) )
+    pm_true <- sapply( xseq , function(a) ( dat$p[m] * ( 1 - exp( -2*a ) ) ) )
     lines( xseq , pm_true , lwd=6 , col="white" )
     lines( xseq , pm_true , lwd=2 , col=2 )
 }
